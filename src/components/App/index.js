ff9db6561b0375cb162f396aa76dfd6422cdf4a6
Roberto <garciamercado.r@gmail.com>
2019-01-20 19:19:44 -0300

    init

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+
+const App = () => (
+    <div>
+        <h1>App</h1>
+        <Button variant="raised" color="primary">
+            Hello World
+        </Button>
+    </div>
+);
+
+export default App;
